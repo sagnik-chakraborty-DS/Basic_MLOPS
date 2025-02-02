8b7a65277f470be0a659eea42fcbdbc66b64b6c5
sagnik-chakraborty-DS <sagnikchakss@gmail.com>
2025-02-02 09:10:30 +0530

    adding docker and workflow

--- app.py
+++ app.py
@@ -39,4 +39,9 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)  
+    app.run(host='0.0.0.0', port=8080)   
+
+
+"""
+docker run -p 8080:8080 sagnikds/studentperformance:latest
+"""
